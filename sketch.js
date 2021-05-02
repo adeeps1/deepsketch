d3277a20e9d573824b74ec3a57a4672318fc9b76
Amardeep Singh <adeeps1508@gmail.com>
2021-05-03 10:06:41 +1200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -152,4 +152,25 @@ document.addEventListener('DOMContentLoaded', () => {
       })
 
       updateControls()
+      function resize() {
+
+    var canvas = document.getElementById('undoCanvas');
+    var canvasRatio = canvas.height / canvas.width;
+    var windowRatio = window.innerHeight / window.innerWidth;
+    var width;
+    var height;
+
+    if (windowRatio < canvasRatio) {
+        height = window.innerHeight;
+        width = height / canvasRatio;
+    } else {
+        width = window.innerWidth;
+        height = width * canvasRatio;
+    }
+
+    canvas.style.width = width + 'px';
+    canvas.style.height = height + 'px';
+}
+
+window.addEventListener('resize', resize, false);
     })
